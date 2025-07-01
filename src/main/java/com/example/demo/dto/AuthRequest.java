9111ecf2d4831f1f0a1616c2eea55936ede03782
Nam Nguyen <Nam_Nguyen@thecdm.ca>
2025-06-30 18:00:12 -0700

    authentication

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,14 +1,12 @@
 package com.example.demo.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class AuthRequest {
-    private String username;
+    private String email;
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
--- src/main/java/com/example/demo/models/BiomarkerValue.java
+++ src/main/java/com/example/demo/models/BiomarkerValue.java
@@ -1,7 +1,12 @@
 package com.example.demo.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
 public class BiomarkerValue {
     private Object value;
     private String resultFlag;
@@ -9,43 +14,4 @@ public class BiomarkerValue {
     private String resultUnits;
     private Date timeResulted;
 
-    public Object getValue() {
-        return value;
-    }
-
-    public void setValue(Object value) {
-        this.value = value;
-    }
-
-    public String getResultFlag() {
-        return resultFlag;
-    }
-
-    public void setResultFlag(String resultFlag) {
-        this.resultFlag = resultFlag;
-    }
-
-    public String getReferenceRange() {
-        return referenceRange;
-    }
-
-    public void setReferenceRange(String referenceRange) {
-        this.referenceRange = referenceRange;
-    }
-
-    public String getResultUnits() {
-        return resultUnits;
-    }
-
-    public void setResultUnits(String resultUnits) {
-        this.resultUnits = resultUnits;
-    }
-
-    public Date getTimeResulted() {
-        return timeResulted;
-    }
-
-    public void setTimeResulted(Date timeResulted) {
-        this.timeResulted = timeResulted;
-    }
 }
